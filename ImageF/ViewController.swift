f7e08d74a74a8c3f57663589c7ae9f780980d15d
DmitryL <d.luzhetsky@gmail.com>
2015-03-30 01:01:32 +0300

    Fixed indent for Image

--- ImageF/ViewController.swift
+++ ImageF/ViewController.swift
@@ -12,17 +12,18 @@ import Foundation
 let apiKey = "2b9d21e095e9d637c900b908c00485a4"
 
 class ViewController:
-UIViewController{
+    UIViewController
+{
     
     @IBOutlet weak var labelTextToView: UILabel!
     var imageView = UIImageView()
     
     private var searches = [FlickrSearchResults]()
     let flickr = Flickr()
-    
+
+//MARK: - Text input Done
     func textFieldShouldReturn(textField: UITextField!) -> Bool {
         self.imageView.removeFromSuperview()
-        
         self.internetChecking()
         
         if (self.labelTextToView.text == ""){
@@ -33,7 +34,6 @@ UIViewController{
             
             textField.placeholder = "searching...\(textField.text)";
             textField.text=checkInputValuesToTextBox(textField.text)
-            
             flickr.searchFlickrForTerm(textField.text) {
                 results, error in
                 
@@ -57,22 +57,7 @@ UIViewController{
         return true
     }
     
-    private func checkInputValuesToTextBox(inputText:NSString)->NSString{
-        var outText:NSString = inputText.stringByReplacingOccurrencesOfString(" ", withString:"")
-        
-        if outText.length==0{
-            outText="\(arc4random()%1000)"
-        }
-        else {outText = inputText
-        }
-        
-        if outText.componentsSeparatedByString(" ").count>4{
-            outText = "\(arc4random()%1000)"
-        }
-        
-        return outText
-    }
-    
+//MARK: - Image
     private func drawImageToView(){
         
         let flickrPhoto = self.photoForIndexPath(0)
@@ -82,79 +67,38 @@ UIViewController{
         
         var _width: Int = Int(imageToShow.size.width)
         var _height:Int = Int(imageToShow.size.height)
-        var _viewSizeWidht: Int = Int(self.view.frame.width)
-        var _viewSizeHeight:Int = Int(self.view.frame.height)
+
+        var _viewSizeWidht: Int = Int(countTrueViewSize().width)
+        var _viewSizeHeight:Int = Int(countTrueViewSize().height)
         
-        if UIDevice.currentDevice().orientation.isLandscape.boolValue {
-            //println("land")
-            if _viewSizeWidht < _viewSizeHeight{
-                var temp = _viewSizeWidht
-                _viewSizeWidht = _viewSizeHeight
-                _viewSizeHeight = temp
-            }
+        if (_width > _viewSizeWidht-20){
+            _width = _viewSizeWidht-20
+            _height=Int(Float(_width)/kof)
         }
-            
-        else {
-            //println("port")
-            if _viewSizeHeight < _viewSizeWidht{
-                var temp = _viewSizeWidht
-                _viewSizeWidht = _viewSizeHeight
-                _viewSizeHeight = temp
-            }
+        if (_height > _viewSizeHeight-100){
+            _height = _viewSizeHeight-100
+            _width = Int(Float(_height)*kof)
         }
-        
-        if kof>1{
-            if Int(imageToShow.size.width) > _viewSizeWidht-25{
-                _width=_viewSizeWidht-25
-                _height=Int(Float(_width)/kof)
-            }
-        }
-        else{
-            if Int(imageToShow.size.height) > _viewSizeHeight-70{
-                _height = _viewSizeHeight-70
-                _width=Int(Float(_height)*kof)
-            }
-        }
-        if (kof)<1.1&&(kof)>0.9{
-            if _viewSizeHeight > _viewSizeWidht{
-                _width=_viewSizeWidht-75
-                _height=_width-1
-            }
-            else {
-                _width=_viewSizeHeight-75
-                _height=_width-1
-            }
-        }
-        println("\(_width) \(_height) \(kof)")
-        
+        println("Размер картинки x:\(_width) y:\(_height) ко:\(kof) screen x:\(_viewSizeWidht) y:\(_viewSizeHeight)")
+
         self.imageView.removeFromSuperview()
         self.imageView.image=imageToShow
         self.imageView.image=makeNewImageWithGoodSize(imageToShow, _width: _width, _height: _height)
         self.imageView.setTranslatesAutoresizingMaskIntoConstraints(false)
         self.view.addSubview(self.imageView)
         
-        var myConstraint = NSLayoutConstraint(item: self.imageView, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 20)
-        self.view.addConstraint(myConstraint)
+        var myConstraint = NSLayoutConstraint(item: self.imageView, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 10)
+       self.view.addConstraint(myConstraint)
         myConstraint = NSLayoutConstraint(item: self.imageView, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0)
         self.view.addConstraint(myConstraint)
         
     }
     
-    private func makeNewImageWithGoodSize (image:UIImage, _width:Int,_height:Int)->UIImage{
-        let image = image.CGImage
-        let width = UInt(_width)
-        let height = UInt(_height)
-        let bitsPerComponent = CGImageGetBitsPerComponent(image)
-        let bytesPerRow = CGImageGetBytesPerRow(image)
-        let colorSpace = CGImageGetColorSpace(image)
-        let bitmapInfo = CGImageGetBitmapInfo(image)
-        let context = CGBitmapContextCreate(nil, width, height, bitsPerComponent, bytesPerRow, colorSpace, bitmapInfo)
-        CGContextSetInterpolationQuality(context, kCGInterpolationHigh)
-        CGContextDrawImage(context, CGRect(origin: CGPointZero, size: CGSize(width: CGFloat(width), height: CGFloat(height))), image)
-        let scaledImage = UIImage(CGImage: CGBitmapContextCreateImage(context))
-        
-        return scaledImage!
+    func photoForIndexPath(index: Int) -> FlickrPhoto {
+        return searches[index].searchResults[index]
     }
+    
+//MARK: - Counts & Chechings-
     
     // iOS > 8.0
     override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
@@ -170,10 +114,6 @@ UIViewController{
         }
     }
     
-    func photoForIndexPath(index: Int) -> FlickrPhoto {
-        return searches[index].searchResults[index]
-    }
-    
     func internetChecking() {
         let internet = Reachability()
         if internet.isConnectedToNetwork(){
@@ -182,5 +122,61 @@ UIViewController{
         else{
             self.labelTextToView.text="Нет инетернета\n=\nНет картинок\n;("
         }
+    }
+
+    
+    func checkInputValuesToTextBox(inputText:NSString)->NSString{
+        var outText:NSString = inputText.stringByReplacingOccurrencesOfString(" ", withString:"")
+        
+        if outText.length==0{
+            outText="\(arc4random()%1000)"
+        }
+        else {outText = inputText
+        }
+        
+        if outText.componentsSeparatedByString(" ").count>4{
+            outText = "\(arc4random()%1000)"
+        }
+        
+        return outText
+    }
+    
+    func countTrueViewSize()->CGSize{
+        
+        var _viewSizeWidht: Int = Int(self.view.frame.width)
+        var _viewSizeHeight:Int = Int(self.view.frame.height)
+        
+        if UIDevice.currentDevice().orientation.isLandscape.boolValue {
+            //println("land")
+            if _viewSizeWidht < _viewSizeHeight{
+                var temp = _viewSizeWidht
+                _viewSizeWidht = _viewSizeHeight
+                _viewSizeHeight = temp
+            }}
+        else {
+            //println("port")
+            if _viewSizeHeight < _viewSizeWidht{
+                var temp = _viewSizeWidht
+                _viewSizeWidht = _viewSizeHeight
+                _viewSizeHeight = temp
+            }}
+        var outSize:CGSize = CGSize(width: _viewSizeWidht, height: _viewSizeHeight)
+        return outSize
+    }
+    
+    func makeNewImageWithGoodSize (image:UIImage, _width:Int,_height:Int)->UIImage{
+        let image = image.CGImage
+        let width = UInt(_width)
+        let height = UInt(_height)
+        let bitsPerComponent = CGImageGetBitsPerComponent(image)
+        let bytesPerRow = CGImageGetBytesPerRow(image)
+        let colorSpace = CGImageGetColorSpace(image)
+        let bitmapInfo = CGImageGetBitmapInfo(image)
+        let context = CGBitmapContextCreate(nil, width, height, bitsPerComponent, bytesPerRow, colorSpace, bitmapInfo)
+        CGContextSetInterpolationQuality(context, kCGInterpolationHigh)
+        CGContextDrawImage(context, CGRect(origin: CGPointZero, size: CGSize(width: CGFloat(width), height: CGFloat(height))), image)
+        let scaledImage = UIImage(CGImage: CGBitmapContextCreateImage(context))
+        
+        return scaledImage!
     }
 }
